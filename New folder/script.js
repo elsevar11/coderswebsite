fb80c05bad47423df23a09049a1afe11d5707a9c
elsevar11 <qasimovelsever11@gmail.com>
2022-04-23 23:23:43 +0400

    first commit

---
+++ New folder/script.js
@@ -0,0 +1,45 @@
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.iconhamburger');
+const navbar = document.querySelector('.navbar');
+const section = document.querySelector('.section');
+const section_2=document.querySelector('.body');
+const tab=document.querySelector('.tab-2');
+
+hamburger.addEventListener('click', () => {
+    section.classList.toggle('sectionactive');
+    section_2.classList.toggle('sectionactive');
+    navbar.classList.toggle('show');
+    hamburger.classList.toggle('active1');
+    menu.classList.toggle('mobilemenu');
+    
+    
+})
+
+
+
+hamburger.addEventListener('click', () => {
+      if(screen.width>=768 && screen.width<1223){
+      section.classList.toggle('sectiontab')
+      }
+    })
+
+
+
+
+    const parentContainer =  document.querySelector('.read-more-container');
+
+    parentContainer.addEventListener('click', event=>{
+    
+        const current = event.target;
+    
+        const isReadMoreBtn = current.className.includes('read-more-btn');
+    
+        if(!isReadMoreBtn) return;
+    
+        const currentText = event.target.parentNode.querySelector('.read-more-text');
+    
+        currentText.classList.toggle('read-more-text--show');
+    
+        current.textContent = current.textContent.includes('Развернуть') ? "свернуть" : "Развернуть...";
+    
+    })
---
+++ about_us/about.js
@@ -0,0 +1,131 @@
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.iconhamburger');
+const navbar = document.querySelector('.navbar');
+const section = document.querySelector('.section');
+const section_2=document.querySelector('.Body');
+const tab=document.querySelector('.tab-2');
+
+hamburger.addEventListener('click', () => {
+    section.classList.toggle('sectionactive');
+    section_2.classList.toggle('sectionactive');
+    navbar.classList.toggle('show');
+    hamburger.classList.toggle('active1');
+    menu.classList.toggle('mobilemenu');
+    
+    
+})
+
+
+
+hamburger.addEventListener('click', () => {
+      if(screen.width>=768 && screen.width<1223){
+      section.classList.toggle('sectiontab')
+      }
+    })
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    // Получаем переменные
+    let arrowLeft = document.getElementById("arrow-left"),
+        arrowRight = document.getElementById("arrow-right"),
+        sliderImg = document.getElementById("slider-img"),
+        dotsWrap = document.getElementsByClassName("slider-dots")[0],
+        dots = document.getElementsByClassName("dot2"),
+        left = -400,
+        imgWidth = 200;
+    
+    // Стартовая позиция слайдера
+    sliderImg.style.left = left + 'px';
+    
+    //Функция перелистывания
+    slideTo = (direction) => {
+      if (direction == "left") {
+        left = left - imgWidth;
+      }
+      
+      if (direction == "right") left += imgWidth;
+      if (left < -800) left = 0;
+      if (left > 0) left =-800;
+      //Убираем все активные классы
+      for (let item of dots) {
+        item.classList.remove("active3");
+      }
+      
+      // Вставляем функцию switch для соединения строелок и точек при переключении стрелками
+      switch (left) {
+        case 0:
+          dots[0].classList.add("active3");
+          break;
+        case -400:
+          dots[1].classList.add("active3");
+          break;
+        case -800:
+          dots[2].classList.add("active3");
+          break;
+      }
+      sliderImg.style.left = left + "px";
+    }
+    
+    // Автоматическое переключение
+      let timerId = setInterval(() => {
+        slideTo("left"); //Направление перелистывания можно поставить right
+      }, 1000);
+      
+    arrowLeft.addEventListener("click", () => {
+      clearInterval(timerId); //Останавливает таймер при клике на левую кнопку
+      slideTo("left");
+    });
+    
+    arrowRight.addEventListener("click", () => {
+      clearInterval(timerId); //Останавливает таймер при клике на правую кнопку
+      slideTo("right");
+    });
+    
+    // Переключение точек 
+    dotsWrap.addEventListener("click", (event) => {
+      clearInterval(timerId); //Останавливает таймер при клике на точки
+      let target = event.target;
+      for (let item of dots) {
+        item.classList.remove("active3");
+      }
+      if(target.classList.contains("dot")) {
+         target.classList.add("active3");
+      }
+      
+      // Перелистывание слайдера при нажании на точку
+      switch (target) {
+        case dots[0]:
+          left = 0;
+          break;
+        case dots[1]:
+          left = -400; // если поставить -200, и следующее -400то листается по одной картинке
+          break;
+        case dots[2]:
+          left = -800; // если поставить -400, и предыдущее -200то листается по одной картинке
+          break;
+      }
+      
+      sliderImg.style.left = left + 'px';
+     
+      
+      })
+    
+    
+    
+    
+     
+      
+
+    
+
+
+
